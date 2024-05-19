f77525a4fce90f7889254cee603feff8a0a977e3
Khushipanwar298 <168843868+Khushipanwar298@users.noreply.github.com>
2024-05-19 18:49:00 +0530

    Add files via upload

---
+++ Week 1 (Task 1)/index.js
@@ -0,0 +1,10 @@
+function showSideBar(){
+    const menuIcon = document.querySelector('.forMobile');
+    menuIcon.style.display = 'flex';
+    
+    }
+    function removeMenu(){
+        const menuIcon = document.querySelector('.forMobile');
+        menuIcon.style.display = 'none';
+        
+        }
